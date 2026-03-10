fc060b109d41c2d93063ce3a7ba7c21cc4972d1a
Lucas Antônio Guimarães Petry <lucas.petry@cnj.jus.br>
2026-03-10 16:57:31 -0300

    Continuação do projeto

---
+++ maratona-devdojo/src/academy/devdojo/maratonajava/javacore/introducaoclasses/dominio/Estudante.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.dominio;
+
+public class Estudante {
+    //classes de domínio mapeiam algo do mundo real para o computacional
+    //possuem os atributos - características
+    //possuem os métodos - ações e comportamentos
+    public String nome;
+    public int idade;
+    public char sexo;
+
+}
